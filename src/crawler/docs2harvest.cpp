45e1cfa04957b26ee79f7193487bb53d9df01a58
Radu Hambasan <radu.hambasan@gmail.com>
2015-06-26 08:20:55 -0700

    Fix docs2harvest param parsing

--- src/crawler/docs2harvest.cpp
+++ src/crawler/docs2harvest.cpp
@@ -69,7 +69,7 @@ int main(int argc, char* argv[]) {
     FlagParser::addFlag('x', "document-uri-xpath", FLAG_OPT, ARG_REQ);
     FlagParser::addFlag('c', "config-file", FLAG_OPT, ARG_REQ);
     FlagParser::addFlag('n', "no-data", FLAG_OPT, ARG_NONE);
-    FlagParser::setMinNumParams(1);
+    FlagParser::setMinNumParams(0);
 
     if (FlagParser::parse(argc, argv) != 0) {
         fprintf(stderr, "%s", FlagParser::getUsage().c_str());
